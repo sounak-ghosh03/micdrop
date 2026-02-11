d24695f32327fb3bff7aeb224d35be308fffa266
SOUNAK GHOSH <sounak2309@gmail.com>
2026-02-11 20:16:11 +0530

    mounted routes

--- server/index.js
+++ server/index.js
@@ -3,27 +3,31 @@ import express from "express";
 // import cookieParser from "cookie-parser";
 import cors from "cors";
 import { DB } from "./config/db.js";
+import reactionRoute from "./routes/reaction.route.js";
+import performanceRoute from "./routes/performance.route.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const corsOptions = {
-  origin: process.env.CLIENT_URL,
-  credentials: true,
+   origin: process.env.CLIENT_URL,
+   credentials: true,
 };
 
 app.use(cors(corsOptions));
 // app.use(cookieParser());
 app.use(express.json());
+app.use("/api/performances", performanceRoute);
+app.use("/api/performances/:id/reactions", reactionRoute);
 
 // Connect to DB
 DB().catch((err) => {
-  console.log("Failed to connect to DB", err);
-  process.exit(1);
+   console.log("Failed to connect to DB", err);
+   process.exit(1);
 });
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+   console.log(`Server running on http://localhost:${PORT}`);
 });
