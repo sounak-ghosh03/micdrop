38c70232e8d2b8673e264b59d8de702679f64cd9
Sourajit007 <workforflash386@gmail.com>
2026-02-20 21:33:37 +0530

    Update role.middleware.js

---
+++ server/middlewares/role.middleware.js
@@ -0,0 +1,15 @@
+export const roleMiddleware = (...allowedRoles) => {
+  return (req, res, next) => {
+    if (!req.user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    if (!allowedRoles.includes(req.user.role)) {
+      return res.status(403).json({
+        message: "Access denied: insufficient permissions",
+      });
+    }
+
+    next();
+  };
+};
