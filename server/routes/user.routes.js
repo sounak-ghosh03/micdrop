26ebbd1eb919bc0749d0484284184df1c3a7e8bb
Sourajit007 <workforflash386@gmail.com>
2026-04-15 16:48:41 +0530

    Add user routes for profile and follow functionality

---
+++ server/routes/user.routes.js
@@ -0,0 +1,24 @@
+// backend/src/routes/user.routes.js
+
+import express from "express";
+import {
+   getUserProfile,
+   followUser,
+   unfollowUser,
+   getFollowers,
+   getFollowing,
+   searchUsers,
+} from "../controllers/user.controller.js";
+
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+
+const router = express.Router();
+
+router.get("/search", searchUsers);
+router.get("/:id", getUserProfile);
+router.post("/:id/follow", authMiddleware, followUser);
+router.delete("/:id/follow", authMiddleware, unfollowUser);
+router.get("/:id/followers", getFollowers);
+router.get("/:id/following", getFollowing);
+
+export default router;
