c20223204938eebe589570335a86c49ef40e350a
SOUNAK GHOSH <sounak2309@gmail.com>
2026-04-21 09:54:07 +0530

    mounted routes on index

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ import cors from "cors";
 import { DB } from "./config/db.js";
 import reactionRoute from "./routes/reaction.route.js";
 import performanceRoute from "./routes/performance.route.js";
+import authRoute from "./routes/auth.route.js";
+import userRoute from "./routes/user.routes.js";
+import commentRoute from "./routes/comment.routes.js";
+import { errorMiddleware } from "./middlewares/error.middleware.js";
 
 dotenv.config();
 
@@ -17,11 +21,17 @@ app.use(
       credentials: true,
    }),
 );
-app.use(express.json({limit: "16kb"}));
+app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 app.use("/api/performances", performanceRoute);
 app.use("/api/performances/:id/reactions", reactionRoute);
+app.use("/api/comments", commentRoute);
+
+// Global error handler
+app.use(errorMiddleware);
 
 // Connect to DB and start server
 DB()
